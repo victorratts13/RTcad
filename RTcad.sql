a704692be2d3ac0a97952eafc808213cf44660a7
Victor Ratts <victor.ratts13@gmail.com>
2019-03-05 15:38:16 -0300

    dataBase
    banco de dados sql

---
+++ RTcad.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Tempo de geração: 03/03/2019 às 16:58
+-- Versão do servidor: 10.1.37-MariaDB-0+deb9u1
+-- Versão do PHP: 7.0.33-0+deb9u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `RTcad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `CDD`
+--
+
+CREATE TABLE `CDD` (
+  `id` int(11) NOT NULL,
+  `depart` varchar(500) NOT NULL,
+  `area` varchar(500) NOT NULL,
+  `data` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `CDG`
+--
+
+CREATE TABLE `CDG` (
+  `id` int(11) NOT NULL,
+  `func` varchar(200) NOT NULL,
+  `descr` varchar(500) NOT NULL,
+  `valor` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `CFDA`
+--
+
+CREATE TABLE `CFDA` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(200) NOT NULL,
+  `sobrenome` varchar(200) NOT NULL,
+  `depart` varchar(500) NOT NULL,
+  `Data` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices de tabelas apagadas
+--
+
+--
+-- Índices de tabela `CDD`
+--
+ALTER TABLE `CDD`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `CDG`
+--
+ALTER TABLE `CDG`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `CFDA`
+--
+ALTER TABLE `CFDA`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas apagadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `CDD`
+--
+ALTER TABLE `CDD`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de tabela `CDG`
+--
+ALTER TABLE `CDG`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de tabela `CFDA`
+--
+ALTER TABLE `CFDA`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
