5a962292105726e63700e6bd6a672f740ef608bd
Victor Ratts <victor.ratts13@gmail.com>
2019-03-06 17:44:21 -0300

    Add files via upload

---
+++ src/content/CH.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Chart from '../ui/Chart'
+
+export default class CH extends Component {
+    
+        render(){
+            return(
+                <div>
+                    <Chart />
+                </div>
+            )
+        }
+}
