260ccf8378c9d0b4b748420ffbde3245a59f1f83
Victor Ratts <victor.ratts13@gmail.com>
2019-03-07 10:48:02 -0300

    Update Chart.js

--- src/ui/Chart.js
+++ src/ui/Chart.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
 import {Bar, Line, Pie} from 'react-chartjs-2';
+var url = "http://localhost:3300/apiGDM";
+
 
 export default class Chart extends Component {
-    constructor(){
-        super();
-        this.state = {
+    state = {
+        getCDG: []
+    }
+    
+    componentDidMount(){
+        this.getData();
+    }
+    getData =_=>{
+        fetch('http://localhost:3300/apiGDM')
+            .then(Response => Response.json())
+            .then(Response => this.setState({getCDG: Response.data}))
+            .catch(err => console.error(err))
+    }
+    
+    test = {
+        
             chartData:{
-                labels: ['data01', 'data02', 'data03', 'data04'],
+                labels: ['getCDG', 'data02', 'data03', 'data04'],
                 datasets: [{
                     label: 'Population',
                     data: [
@@ -33,19 +48,59 @@ export default class Chart extends Component {
             }
         }
 
-    }
     
         render(){
+            var id;
             return(
+                
                 <div className="chart">
+                {this.state.getCDG.map((getCDG) => { return( 
+                <div className="card">
+                <div className="card">Gráfico</div>
                     <Bar
-                    data={this.state.chartData}
+                        
+                    data={{
+                        labels: [`Lucro`, 'DPF', 'Prej.', 'IDG'],
+                        datasets: [{
+                            label: `${getCDG.depart}`,
+                            data: [
+                                `${getCDG.valor}`,
+                                `${getCDG.valor / getCDG.id}`,
+                                `${getCDG.valor * 1/2}`,
+                                `${getCDG.valor * 10/100}`
+                            ],
+                            backgroundColor:[
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)'
+                            ],
+                            borderColor:[
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)'
+                            ],
+                            borderWidth: '1'
+        
+                        }]
+                    }}
                     width={100}
-                    height={50}
+                    height={100}
                     options={{
                         maintainAspectRatio:true
                     }}
+                    
                     />
+                    
+                    <div>
+                    
+              
+    
+            
+                    </div>
+                </div>
+                )})}
                 </div>
             )
         }
