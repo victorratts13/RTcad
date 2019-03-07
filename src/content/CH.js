385abeeeae520ebb2d1f03d86dc5caf10160475f
Victor Ratts <victor.ratts13@gmail.com>
2019-03-07 10:48:36 -0300

    Update CH.js

--- src/content/CH.js
+++ src/content/CH.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import Chart from '../ui/Chart'
+import ChartPie from '../ui/ChartPie'
 
 export default class CH extends Component {
     
         render(){
             return(
-                <div>
+                <div className="nav container-fluid text-center">
+                    <div className="card">
+                        <h2>Detalhamento Gráfico<br /> e movimentação</h2>
+                        <h5>Verifique o Lucro, dPF (despesa por funcionario), IDG (indice de desenvolvimento geral) e prejuizo de cada departamento</h5>
+                    </div>
+                    <div style={{
+                        width: '350px',
+                        height: '500px',
+                        backgroundColor: 'rgba(50,255,50,0.01)'}}>
                     <Chart />
+                    </div>    
                 </div>
             )
         }
